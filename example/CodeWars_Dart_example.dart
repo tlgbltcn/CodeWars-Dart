d976ff6b74fbc7eb23463122ed035101c69c4a69
tolga.bolatcan <tbolatcan@gmail.com>
2019-03-29 11:24:54 +0300

    create repository

---
+++ example/CodeWars_Dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:CodeWars_Dart/CodeWars_Dart.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/CodeWars_Dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library CodeWars_Dart;
+
+export 'src/CodeWars_Dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
